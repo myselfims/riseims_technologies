8fde35e92ef9b951808b2320f1f5a579769aa7ef
myselfims <shaikhimran7585@gmail.com>
2023-04-27 15:50:36 +0530

    url change for static

--- riseimssite/settings.py
+++ riseimssite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_HOST = "https://www.riseimstechnologies.com" if not DEBUG else ""
+STATIC_HOST = "https://site.riseimstechnologies.com/" if not DEBUG else ""
 STATIC_URL = STATIC_HOST + "/static/"
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
